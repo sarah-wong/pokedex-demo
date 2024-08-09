f1253bf80892c2da6edc2e54a6d3f2d5ddf84f47
PerScholarSarah <rmandolin22@gmail.com>
2024-08-09 15:41:13 -0400

    cards loaded

--- app.js
+++ app.js
@@ -1,16 +1,612 @@
 const imgDomain = "https://img.pokemondb.net/artwork/avif/";
 const imgType = ".avif";
 
-let pokemonData = getData();
-console.log(pokemonData);
-
-async function getData(){
-    const file = await fetch("/data.json");
-    console.log(file); // doesn't work without the clogs
-    const json = await file.json();
-    console.log(json); //DO NOT REMOVE!!
-    return json;
-}
+let pokemonData = [
+    {
+      "name": "Bulbasaur",
+      "type": "Grass,Poison"
+    },
+    {
+      "name": "Ivysaur",
+      "type": "Grass,Poison"
+    },
+    {
+      "name": "Venusaur",
+      "type": "Grass,Poison"
+    },
+    {
+      "name": "Charmander",
+      "type": "Fire"
+    },
+    {
+      "name": "Charmeleon",
+      "type": "Fire"
+    },
+    {
+      "name": "Charizard",
+      "type": "Fire,Flying"
+    },
+    {
+      "name": "Squirtle",
+      "type": "Water"
+    },
+    {
+      "name": "Wartortle",
+      "type": "Water"
+    },
+    {
+      "name": "Blastoise",
+      "type": "Water"
+    },
+    {
+      "name": "Caterpie",
+      "type": "Bug"
+    },
+    {
+      "name": "Metapod",
+      "type": "Bug"
+    },
+    {
+      "name": "Butterfree",
+      "type": "Bug,Flying"
+    },
+    {
+      "name": "Weedle",
+      "type": "Bug,Poison"
+    },
+    {
+      "name": "Kakuna",
+      "type": "Bug,Poison"
+    },
+    {
+      "name": "Beedrill",
+      "type": "Bug,Poison"
+    },
+    {
+      "name": "Pidgey",
+      "type": "Normal,Flying"
+    },
+    {
+      "name": "Pidgeotto",
+      "type": "Normal,Flying"
+    },
+    {
+      "name": "Pidgeot",
+      "type": "Normal,Flying"
+    },
+    {
+      "name": "Rattata",
+      "type": "Normal"
+    },
+    {
+      "name": "Raticate",
+      "type": "Normal"
+    },
+    {
+      "name": "Spearow",
+      "type": "Normal,Flying"
+    },
+    {
+      "name": "Fearow",
+      "type": "Normal,Flying"
+    },
+    {
+      "name": "Ekans",
+      "type": "Poison"
+    },
+    {
+      "name": "Arbok",
+      "type": "Poison"
+    },
+    {
+      "name": "Pikachu",
+      "type": "Electric"
+    },
+    {
+      "name": "Raichu",
+      "type": "Electric"
+    },
+    {
+      "name": "Sandshrew",
+      "type": "Ground"
+    },
+    {
+      "name": "Sandslash",
+      "type": "Ground"
+    },
+    {
+      "name": "Nidoran♀",
+      "type": "Poison"
+    },
+    {
+      "name": "Nidorina",
+      "type": "Poison"
+    },
+    {
+      "name": "Nidoqueen",
+      "type": "Poison,Ground"
+    },
+    {
+      "name": "Nidoran♂",
+      "type": "Poison"
+    },
+    {
+      "name": "Nidorino",
+      "type": "Poison"
+    },
+    {
+      "name": "Nidoking",
+      "type": "Poison,Ground"
+    },
+    {
+      "name": "Clefairy",
+      "type": "Normal"
+    },
+    {
+      "name": "Clefable",
+      "type": "Normal"
+    },
+    {
+      "name": "Vulpix",
+      "type": "Fire"
+    },
+    {
+      "name": "Ninetales",
+      "type": "Fire"
+    },
+    {
+      "name": "Jigglypuff",
+      "type": "Normal"
+    },
+    {
+      "name": "Wigglytuff",
+      "type": "Normal"
+    },
+    {
+      "name": "Zubat",
+      "type": "Poison,Flying"
+    },
+    {
+      "name": "Golbat",
+      "type": "Poison,Flying"
+    },
+    {
+      "name": "Oddish",
+      "type": "Grass,Poison"
+    },
+    {
+      "name": "Gloom",
+      "type": "Grass,Poison"
+    },
+    {
+      "name": "Vileplume",
+      "type": "Grass,Poison"
+    },
+    {
+      "name": "Paras",
+      "type": "Bug,Grass"
+    },
+    {
+      "name": "Parasect",
+      "type": "Bug,Grass"
+    },
+    {
+      "name": "Venonat",
+      "type": "Bug,Poison"
+    },
+    {
+      "name": "Venomoth",
+      "type": "Bug,Poison"
+    },
+    {
+      "name": "Diglett",
+      "type": "Ground"
+    },
+    {
+      "name": "Dugtrio",
+      "type": "Ground"
+    },
+    {
+      "name": "Meowth",
+      "type": "Normal"
+    },
+    {
+      "name": "Persian",
+      "type": "Normal"
+    },
+    {
+      "name": "Psyduck",
+      "type": "Water"
+    },
+    {
+      "name": "Golduck",
+      "type": "Water"
+    },
+    {
+      "name": "Mankey",
+      "type": "Fighting"
+    },
+    {
+      "name": "Primeape",
+      "type": "Fighting"
+    },
+    {
+      "name": "Growlithe",
+      "type": "Fire"
+    },
+    {
+      "name": "Arcanine",
+      "type": "Fire"
+    },
+    {
+      "name": "Poliwag",
+      "type": "Water"
+    },
+    {
+      "name": "Poliwhirl",
+      "type": "Water"
+    },
+    {
+      "name": "Poliwrath",
+      "type": "Water,Fighting"
+    },
+    {
+      "name": "Abra",
+      "type": "Psychic"
+    },
+    {
+      "name": "Kadabra",
+      "type": "Psychic"
+    },
+    {
+      "name": "Alakazam",
+      "type": "Psychic"
+    },
+    {
+      "name": "Machop",
+      "type": "Fighting"
+    },
+    {
+      "name": "Machoke",
+      "type": "Fighting"
+    },
+    {
+      "name": "Machamp",
+      "type": "Fighting"
+    },
+    {
+      "name": "Bellsprout",
+      "type": "Grass,Poison"
+    },
+    {
+      "name": "Weepinbell",
+      "type": "Grass,Poison"
+    },
+    {
+      "name": "Victreebel",
+      "type": "Grass,Poison"
+    },
+    {
+      "name": "Tentacool",
+      "type": "Water,Poison"
+    },
+    {
+      "name": "Tentacruel",
+      "type": "Water,Poison"
+    },
+    {
+      "name": "Geodude",
+      "type": "Rock,Ground"
+    },
+    {
+      "name": "Graveler",
+      "type": "Rock,Ground"
+    },
+    {
+      "name": "Golem",
+      "type": "Rock,Ground"
+    },
+    {
+      "name": "Ponyta",
+      "type": "Fire"
+    },
+    {
+      "name": "Rapidash",
+      "type": "Fire"
+    },
+    {
+      "name": "Slowpoke",
+      "type": "Water,Psychic"
+    },
+    {
+      "name": "Slowbro",
+      "type": "Water,Psychic"
+    },
+    {
+      "name": "Magnemite",
+      "type": "Electric"
+    },
+    {
+      "name": "Magneton",
+      "type": "Electric"
+    },
+    {
+      "name": "Farfetch'd",
+      "type": "Normal,Flying"
+    },
+    {
+      "name": "Doduo",
+      "type": "Normal,Flying"
+    },
+    {
+      "name": "Dodrio",
+      "type": "Normal,Flying"
+    },
+    {
+      "name": "Seel",
+      "type": "Water"
+    },
+    {
+      "name": "Dewgong",
+      "type": "Water,Ice"
+    },
+    {
+      "name": "Grimer",
+      "type": "Poison"
+    },
+    {
+      "name": "Muk",
+      "type": "Poison"
+    },
+    {
+      "name": "Shellder",
+      "type": "Water"
+    },
+    {
+      "name": "Cloyster",
+      "type": "Water,Ice"
+    },
+    {
+      "name": "Gastly",
+      "type": "Ghost,Poison"
+    },
+    {
+      "name": "Haunter",
+      "type": "Ghost,Poison"
+    },
+    {
+      "name": "Gengar",
+      "type": "Ghost,Poison"
+    },
+    {
+      "name": "Onix",
+      "type": "Rock,Ground"
+    },
+    {
+      "name": "Drowzee",
+      "type": "Psychic"
+    },
+    {
+      "name": "Hypno",
+      "type": "Psychic"
+    },
+    {
+      "name": "Krabby",
+      "type": "Water"
+    },
+    {
+      "name": "Kingler",
+      "type": "Water"
+    },
+    {
+      "name": "Voltorb",
+      "type": "Electric"
+    },
+    {
+      "name": "Electrode",
+      "type": "Electric"
+    },
+    {
+      "name": "Exeggcute",
+      "type": "Grass,Psychic"
+    },
+    {
+      "name": "Exeggutor",
+      "type": "Grass,Psychic"
+    },
+    {
+      "name": "Cubone",
+      "type": "Ground"
+    },
+    {
+      "name": "Marowak",
+      "type": "Ground"
+    },
+    {
+      "name": "Hitmonlee",
+      "type": "Fighting"
+    },
+    {
+      "name": "Hitmonchan",
+      "type": "Fighting"
+    },
+    {
+      "name": "Lickitung",
+      "type": "Normal"
+    },
+    {
+      "name": "Koffing",
+      "type": "Poison"
+    },
+    {
+      "name": "Weezing",
+      "type": "Poison"
+    },
+    {
+      "name": "Rhyhorn",
+      "type": "Ground,Rock"
+    },
+    {
+      "name": "Rhydon",
+      "type": "Ground,Rock"
+    },
+    {
+      "name": "Chansey",
+      "type": "Normal"
+    },
+    {
+      "name": "Tangela",
+      "type": "Grass"
+    },
+    {
+      "name": "Kangaskhan",
+      "type": "Normal"
+    },
+    {
+      "name": "Horsea",
+      "type": "Water"
+    },
+    {
+      "name": "Seadra",
+      "type": "Water"
+    },
+    {
+      "name": "Goldeen",
+      "type": "Water"
+    },
+    {
+      "name": "Seaking",
+      "type": "Water"
+    },
+    {
+      "name": "Staryu",
+      "type": "Water"
+    },
+    {
+      "name": "Starmie",
+      "type": "Water,Psychic"
+    },
+    {
+      "name": "Mr. Mime",
+      "type": "Psychic"
+    },
+    {
+      "name": "Scyther",
+      "type": "Bug,Flying"
+    },
+    {
+      "name": "Jynx",
+      "type": "Ice,Psychic"
+    },
+    {
+      "name": "Electabuzz",
+      "type": "Electric"
+    },
+    {
+      "name": "Magmar",
+      "type": "Fire"
+    },
+    {
+      "name": "Pinsir",
+      "type": "Bug"
+    },
+    {
+      "name": "Tauros",
+      "type": "Normal"
+    },
+    {
+      "name": "Magikarp",
+      "type": "Water"
+    },
+    {
+      "name": "Gyarados",
+      "type": "Water,Flying"
+    },
+    {
+      "name": "Lapras",
+      "type": "Water,Ice"
+    },
+    {
+      "name": "Ditto",
+      "type": "Normal"
+    },
+    {
+      "name": "Eevee",
+      "type": "Normal"
+    },
+    {
+      "name": "Vaporeon",
+      "type": "Water"
+    },
+    {
+      "name": "Jolteon",
+      "type": "Electric"
+    },
+    {
+      "name": "Flareon",
+      "type": "Fire"
+    },
+    {
+      "name": "Porygon",
+      "type": "Normal"
+    },
+    {
+      "name": "Omanyte",
+      "type": "Rock,Water"
+    },
+    {
+      "name": "Omastar",
+      "type": "Rock,Water"
+    },
+    {
+      "name": "Kabuto",
+      "type": "Rock,Water"
+    },
+    {
+      "name": "Kabutops",
+      "type": "Rock,Water"
+    },
+    {
+      "name": "Aerodactyl",
+      "type": "Rock,Flying"
+    },
+    {
+      "name": "Snorlax",
+      "type": "Normal"
+    },
+    {
+      "name": "Articuno",
+      "type": "Ice,Flying"
+    },
+    {
+      "name": "Zapdos",
+      "type": "Electric,Flying"
+    },
+    {
+      "name": "Moltres",
+      "type": "Fire,Flying"
+    },
+    {
+      "name": "Dratini",
+      "type": "Dragon"
+    },
+    {
+      "name": "Dragonair",
+      "type": "Dragon"
+    },
+    {
+      "name": "Dragonite",
+      "type": "Dragon,Flying"
+    },
+    {
+      "name": "Mewtwo",
+      "type": "Psychic"
+    },
+    {
+      "name": "Mew",
+      "type": "Psychic"
+    }
+  ];
 
 function generateCard(index, pokemon, digits = 3){
     const len = String(index).length;
@@ -19,7 +615,7 @@ function generateCard(index, pokemon, digits = 3){
     const typeList = pokemon["type"].split(",");
 
     const card = document.createElement("div");
-    newCard.classList.add("card");
+    card.classList.add("card");
 
     const cardPic = card.appendChild(document.createElement("img"));
     cardPic.classList.add("cardPic");
@@ -29,14 +625,24 @@ function generateCard(index, pokemon, digits = 3){
     const cardInfo = card.appendChild(document.createElement("div"));
     cardInfo.classList.add("cardInfo");
     const numElem = cardInfo.appendChild(document.createElement("h1"));
+    numElem.classList.add("number");
     numElem.textContent = numStr;
     const nameElem = cardInfo.appendChild(document.createElement("h3"));
+    nameElem.classList.add("name");
     nameElem.textContent = name;
 
     for (const typeStr of typeList){
         const typeCard = card.appendChild(document.createElement("div"));
-        typeCard.classList.push(typeStr.toLowerCase());
-        typeCard.classList.push("type");
+        typeCard.classList.add(typeStr.toLowerCase(),"type");
         typeCard.textContent = typeStr.toUpperCase();
     }
+    return card;
+}
+
+
+const container = document.querySelector(".cardContainer");
+for (let i = 0; i < pokemonData.length; i++) {
+    console.log(`loading pokemon #${i+1}: ${pokemonData[i]["name"]}`);
+    const card = generateCard(i+1,pokemonData[i]);
+    container.appendChild(card);
 }
